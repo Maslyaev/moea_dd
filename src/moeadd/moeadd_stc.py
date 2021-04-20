3e06aa45db6eb39d904191e618862dc24bfbd06e
Maslyaev <38224722+Maslyaev@users.noreply.github.com>
2021-04-20 19:00:23 +0300

    Add files via upload

--- src/moeadd/moeadd_stc.py
+++ src/moeadd/moeadd_stc.py
@@ -1,4 +1,33 @@
-# -*- coding: utf-8 -*-
+'''
+
+Superclasses and examples of objects, that should be used in the optimization procedures, 
+using moeadd algorithm.
+
+Contains:
+------------
+
+**moeadd_solution** : superclass for the case-specific implementation of the candidate 
+solution for moeadd algorithm.
+
+**moe_population_constructor** : superclass for population constructor object, dedicated 
+to the creation of initial population for the evolutionary algorithm.
+
+**moe_evolutionary_operator** : superclass for evolutionary operator, dedicated to altering the 
+population during evolutionary search. Specific implementations must contain both mutation and 
+crossover suboperators.
+
+**gaussian_mutation** : an example of the mutation suboperator for problems, where 
+genes candidate genes are represented by flowting point strings, that operates by addition 
+of an increment, randomly selected from the normal distribution. 
+
+**mixing_xover** : crossover for problems, involving candidate solutions with genes, 
+represented by strings of floating point values. The offspring genes' elements are selected in 
+the interval between the parents' gene values.
+
+**plot_pareto** : supplementary function to plot the candidate solutions of 2D-problem 
+in form of non-dominated levels, using matplotlib tools.
+
+'''
 
 import numpy as np
 from src.moeadd.moeadd import *
@@ -13,6 +42,42 @@ class moeadd_solution(ABC):
     Abstract superclass of the moeadd solution. *__hash__* method must be declared in the subclasses. 
     Overloaded *__eq__* method of moeadd_solution uses strict equatlity between self.vals attributes,
     therefore, can not be used with the real-valued strings.
+    
+    Parameters:
+    ----------
+    
+    x : arbitrary object, 
+        An arbitrary object, representing the solution gene. For example, 
+        it can be a string of floating-point values, implemented as np.ndarray
+        
+    obj_funs : list of functions
+        Objective functions, that would be optimized by the 
+        evolutionary algorithm.
+    
+    Attributes:
+    -----------
+    
+    vals : arbitrary object
+        An arbitrary object, representing the solution gene.
+        
+    obj_funs : list of functions
+        Objective functions, that would be optimized by the 
+        evolutionary algorithm.
+        
+    precomputed_value : bool
+        Indicator, if the value of the objective functions is already calculated.
+        Implemented to avoid redundant computations.
+        
+    precomputed_domain : bool
+        Indicator, if the solution has been already placed in a domain in objective function 
+        space. Implemented to avoid redundant computations during the point 
+        placement.
+    
+    obj_fun : np.array
+        Property, that calculates/contains calculated value of objective functions.
+        
+    _domain : int
+        Index of the domain, to that the solution belongs.
     
     '''
     def __init__(self, x, obj_funs):
@@ -57,7 +122,7 @@ class moe_population_constructor(ABC):
     '''
     
     Abstract class of the creator of new moeadd solutions, utilized in its initialization phase. 
-    Shall be overwritten to be properly used.
+    Shall be overwritten to be properly used for each specific case.
     
     Methods:
     ---------
@@ -69,6 +134,8 @@ class moe_population_constructor(ABC):
         Method, dedicated to the creation (oftenly randmized) of new candidate solutions.
         
     Example of the subclass:
+    ------------------------
+    
         >>> class test_population_constructor(object):
         >>>     def __init__(self, bitstring_len = 2, vals_range = [-4, 4]):
         >>>         self.bs_len = bitstring_len; self.vals_range = vals_range
@@ -79,7 +146,7 @@ class moe_population_constructor(ABC):
         >>>                                                                 size = self.bs_len), 
         >>>                              obj_funs=[optimized_fun_1, optimized_fun_2])
         >>>         return created_solution        
-    -------
+
     '''
     def __init__(self, *constr_args):
         pass
@@ -187,7 +254,7 @@ def simple_selector(sorted_neighbors, number_of_neighbors = 4):
     return sorted_neighbors[:number_of_neighbors]
 
 
-def plot_pareto(levels, weights = None, max_level = None):
+def plot_pareto(levels, weights = None, max_level = None, logscale = (False, False)):
     '''
     
     Vizualization method to demonstrate the pareto levels of 2D-problem on the plane via matplotlib
@@ -198,30 +265,57 @@ def plot_pareto(levels, weights = None, max_level = None):
     levels : src.moeadd.pareto_levels obj
         object, containing pareto levels. Ususally obtained from *src.moeadd.moeadd_optimizer* attribute *src.moeadd.moeadd_optimizer.pareto_levels*.
         
-    weights : np.ndarray, optional
-        contains weights from the moeadd algorithm to be visualized
+    weights : np.ndarray or None, optional
+        Contains weights from the moeadd algorithm to be visualized. If None, no weights 
+        vectors will be visualized. 
+    
+    max_level : int or None, optonal, default None
+        Number of layers to be visualized during on the plot. If None, all
+        layers will be shown.
+        
+    logscale : tuple of 2 bool values, optional, default ``(False, False)``
+        Indication, of will the axis use logscale on the plot, if all values are positive. 
+        First element of tuple - if the x-axis will use logscale. Second - y-scale.
+        
     
     '''
     if max_level is None:
         max_level = np.inf
     else:
         max_level += 1
+    assert levels.population[0].obj_fun.size == 2
     print([front_idx for front_idx in np.arange(min((len(levels.levels), max_level)))])
     coords = [[(solution.obj_fun[0], solution.obj_fun[1]) for solution in levels.levels[front_idx]] for front_idx in np.arange(min((len(levels.levels), max_level)))]
     coords_arrays = []
     for coord_set in coords:
         coords_arrays.append(np.array(coord_set))
     coords_arrays
-    colors = ['r', 'r', 'b', 'y', 'g'] + ['m' for idx in np.arange(len(coords_arrays) - 5)]
+    colors = ['r', 'k', 'b', 'y', 'g'] + ['m' for idx in np.arange(len(coords_arrays) - 5)]
+    
+    obj_funs_min = np.array([min([sol.obj_fun[ofi] for sol in levels.population]) #Некрасиво
+                        for ofi in range(levels.population[0].obj_fun.size)])
+    obj_funs_max = np.array([max([sol.obj_fun[ofi] for sol in levels.population]) 
+                        for ofi in range(levels.population[0].obj_fun.size)])
+    
+    boundaries_min = obj_funs_min - 0.5*obj_funs_min
+    boundaries_max = obj_funs_max + 0.1*(obj_funs_max - obj_funs_min)
+    
+    
     fig, ax = plt.subplots()
     for front_idx in np.arange(len(coords_arrays)):
-        ax.plot([1.04862679e-18, 1.29891486e-14, 1.30104261e-11, 0.00401406], [6, 2, 1, 0], color = 'r', linewidth = 1)
-        ax.scatter(coords_arrays[front_idx][:, 0], coords_arrays[front_idx][:, 1], color = colors[front_idx], s = 5)
+#        ax.plot([1.04862679e-18, 1.29891486e-14, 1.30104261e-11, 0.00401406], [6, 2, 1, 0], color = 'r', linewidth = 1)
+        ax.scatter(coords_arrays[front_idx][:, 0], coords_arrays[front_idx][:, 1], color = colors[front_idx], s = 20)
         plt.xlabel('Quality')
         plt.ylabel('Complexity')
 #        ax.set_yscale('log')
-        ax.set_xscale('log')
-        plt.xlim((1e-19, 0.006))
+        if boundaries_min[0] > 0 and logscale[0]:
+            ax.set_xscale('log')
+        if boundaries_min[1] > 0 and logscale[1]:
+            ax.set_yscale('log')
+        
+        plt.xlim((boundaries_min[0], boundaries_max[0]))
+        plt.ylim((boundaries_min[1], boundaries_max[1]))
+
         plt.grid()
     if not (weights is None):
         for weight_idx in np.arange(weights.shape[0]):
